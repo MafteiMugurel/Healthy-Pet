a0cfc37538703537aa327321af0a2cbcf2e551a9
MafteiMugurel <mugurel.maftei95@gmail.com>
2025-10-23 17:54:53 +0300

    Single card  with no style

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,19 +1,30 @@
 import "./dashboard.scss";
+import plusIcon from "../../assets/plus.svg";
+import syringeIcon from "../../assets/syringe.svg";
+import cakeIcon from "../../assets/cake.svg";
+import calendarIcon from "../../assets/calendar.svg";
 
 const Dashboard = () => {
   return (
     <div>
       <div>
-        <img src="../../assets/plus.svg" alt="Dog Placeholder" />
-        <p>Paco</p>
+        <img src={plusIcon} alt="PlusIcon" />
+        <p>Name your pet</p>
       </div>
 
-      <img src="dog-placeholder.png" alt="Dog Placeholder" />
-      <p>Paco</p>
-      <img src="dog-placeholder.png" alt="Dog Placeholder" />
-      <p>Paco</p>
-      <img src="dog-placeholder.png" alt="Dog Placeholder" />
-      <p>Paco</p>
+      <div>
+        <img src={cakeIcon} alt="CakeIcon" />
+        <p>How old</p>
+        <img src={calendarIcon} alt="CalendarIcon" />
+        <p>Calendar</p>
+        <img src={syringeIcon} alt="SyringeIcon" />
+        <p>Vaccine</p>
+      </div>
+
+      <div>
+        <button>View</button>
+        <button>Add Record</button>
+      </div>
     </div>
   );
 };
