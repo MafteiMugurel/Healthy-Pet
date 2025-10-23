8d465665b267c648836b14b0a7692560aff7364d
Adicristian1 <istrateadicristian@gmail.com>
2025-10-23 18:05:31 +0300

    Started homepage
    Started homepage

--- src/pages/homepage/homepage.tsx
+++ src/pages/homepage/homepage.tsx
@@ -1,11 +1,26 @@
-
+import "./homepage.scss";
+import imageRight from "./../../assets/homepage-img.png";
 
 const Homepage = () => {
- 
-  return <div>Welcome to the Homepage!</div>;
-
-    
-
+  return (
+    <div className="homepage-container">
+      <div>
+        <div>
+          <div>Welcome to</div>
+          <div>HealthyPet</div>
+          <div>Your safe place which helps you help the others.</div>
+        </div>
+        <div>
+          <button>Login</button>
+          into your account, or
+          <button>Register</button>
+        </div>
+      </div>
+      <div>
+        <img src={imageRight} />
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
