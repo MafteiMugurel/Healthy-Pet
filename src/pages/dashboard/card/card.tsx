ac0560a01eba43d38ed39570e977feb7905ebbfa
MafteiMugurel <mugurel.maftei95@gmail.com>
2025-10-30 11:32:50 +0200

    change className writing rule

--- src/pages/dashboard/card/card.tsx
+++ src/pages/dashboard/card/card.tsx
@@ -6,31 +6,31 @@ import "./card.scss";
 const Card = () => {
   return (
     <div className="card-container">
-      <div className="card-container-header">
+      <div className="card-container__header">
         <img
           src={plusIcon}
           alt="PlusIcon"
-          className="card-container-header-image"
+          className="card-container__header__image"
         />
         <p>Name your pet</p>
       </div>
 
-      <div className="card-container-info">
-        <div className="card-container-info-item">
+      <div className="card-container__info">
+        <div className="card-container__info__item">
           <img src={cakeIcon} alt="CakeIcon" />
           <p>How old</p>
         </div>
-        <div className="card-container-info-item">
+        <div className="card-container__info__item">
           <img src={calendarIcon} alt="CalendarIcon" />
           <p>Calendar</p>
         </div>
-        <div className="card-container-info-item">
+        <div className="card-container__info__item">
           <img src={syringeIcon} alt="SyringeIcon" />
           <p>Vaccine</p>
         </div>
       </div>
 
-      <div className="card-container-actions">
+      <div className="card-container__actions">
         <button>View</button>
         <button>Add Record</button>
       </div>
