d05cd7daa3036caaa8c96dde6884a04793ce02fe
Adicristian1 <istrateadicristian@gmail.com>
2025-11-02 02:12:51 +0200

    added the header and the footer.Styled the form,the header and the footer.

--- src/pages/add-animal/add-animal.tsx
+++ src/pages/add-animal/add-animal.tsx
@@ -17,21 +17,30 @@ import SVGIcon from "../../components/svg-icon/svg-icon";
 const AddAnimal = () => {
   let gender;
   const animalTypes = [
-    { id: "cat", label: "Cat", icon: "cat" },
-    { id: "dog", label: "Dog", icon: "dog" },
-    { id: "bird", label: "Bird", icon: "bird" },
-    { id: "rabbit", label: "Rabbit", icon: "rabbit" },
-    { id: "reptile", label: "Reptile", icon: "reptile" },
-    { id: "turtle", label: "Turtle", icon: "turtle" },
-    { id: "other", label: "Other", icon: "question-mark" },
+    { id: "cat", icon: "cat" },
+    { id: "dog", icon: "dog" },
+    { id: "bird", icon: "bird" },
+    { id: "rabbit", icon: "rabbit" },
+    { id: "reptile", icon: "reptile" },
+    { id: "turtle", icon: "turtle" },
+    { id: "other", icon: "question-mark" },
   ];
   const [selectedAnimal, setSelectedAnimal] = useState({} as any);
 
   return (
     <div className="add-animal-container">
-      <div className="add-animal-container__title">Add new animal</div>
-      <div className="add-animal-container__description">
-        What type of animal it is? Select one of the options
+      <div className="add-animal-header">
+        <div className="add-animal-header__topbar">
+          <div className="add-animal-header__topbar__logo">
+            <h1>HealthyPet</h1>
+          </div>
+        </div>
+        <div className="add-animal-header__content">
+          <h2 className="add-animal-header__content__title">
+            Add new animal form
+          </h2>
+          <p>What type of animal it is? Select one of the questions</p>
+        </div>
       </div>
       <div className="add-animal-container__animal-select">
         {animalTypes.map((animal) => (
@@ -46,31 +55,42 @@ const AddAnimal = () => {
             onClick={() => setSelectedAnimal(animal)}
           >
             <SVGIcon type={animal.icon} />
-            <div className="animal-option__label">{animal.label}</div>
           </div>
         ))}
       </div>
       <form className="add-animal-container__animal-form">
-        <TextField required label="Animal name" />
+        <TextField
+          required
+          label="Animal name"
+          className="field-single"
+          fullWidth
+        />
         <TextField required label="Breed" />
-        <TextField label="Weight" />
         <TextField label="Species" />
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DatePicker />
+          <DatePicker label="Birth date" />
         </LocalizationProvider>
-        <TextField label="Coloring" />
-        <TextField label="Microchip/ID" />
-        <FormControl fullWidth>
+        <FormControl>
           <InputLabel>Gender</InputLabel>
           <Select value={gender} label="Gender">
             <MenuItem value="male">Male</MenuItem>
             <MenuItem value="female">Female</MenuItem>
           </Select>
         </FormControl>
+        <TextField label="Weight" />
+        <TextField label="Coloring" />
+        <TextField label="Microchip / ID number" className="full-width" />
       </form>
-      <div className="add-animal-container__buttons">
-        <Button>ADD MEDICAL RECORDS</Button>
-        <Button>SAVE AND EXIT</Button>
+
+      <div className="add-animal-footer">
+        <p className="add-animal-footer__text">Next, you have to</p>
+        <Button className="add-animal-footer__btn primary">
+          ADD MEDICAL RECORDS
+        </Button>
+        <p className="add-animal-footer__text"> or just </p>
+        <Button className="add-animal-footer__btn secondary">
+          SAVE AND EXIT
+        </Button>
       </div>
     </div>
   );
